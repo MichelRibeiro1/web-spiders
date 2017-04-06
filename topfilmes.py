0dde637ea8eba8ab66f07eb15224dbc3ed06009d
Michel Ribeiro <MichelRibeiro1@users.noreply.github.com>
2017-04-06 17:22:26 -0300

    Atualização no laço, for.

--- topfilmes.py
+++ topfilmes.py
@@ -8,9 +8,8 @@ def filmes_em_cartaz(top_filmes):
 	html = urlopen("http://www.adorocinema.com/filmes/numero-cinemas/")
 	bsobj = BeautifulSoup(html, "lxml")  
 	filmes = bsobj.findAll("a", {"class": "no_underline","href": re.compile("^/filmes/filme(\-)([0-9])+/$")})
-	for filme in range(1,int(top_filmes)+1):
-		print(str(filme) +"-" +filmes[filme].get_text().strip())
+	for filme in range(0,int(top_filmes)):
+		print(str(filme+1) +"-" +filmes[filme].get_text().strip())
 
 top = input('Qual o top de filmes em cartaz deseja visualizar? ')
 filmes_em_cartaz(top)
-
